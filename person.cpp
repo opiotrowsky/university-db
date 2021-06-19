ec064c9e1fb09e2c6b1ddd6ee30ac9a303ece74d
Ireneusz Bohatyrewicz <ibohatyrewicz@gmail.com>
2021-06-19 18:05:01 +0200

    modified the Person and Student classes

--- person.cpp
+++ person.cpp
@@ -1,1 +1,30 @@
 #include "person.hpp"
+
+#include <utility>
+
+Person::Person(std::string name,
+       std::string surname,
+       std::string address,
+       int64_t pesel,
+       Gender sex)
+: name_(std::move(name)), surname_(std::move(surname)), address_(std::move(address)), pesel_(pesel), sex_(sex)
+{
+}
+
+std::string Person::getName() const {
+  return name_;
+}
+
+std::string Person::getSurname() const {
+  return surname_;
+}
+
+std::string Person::getAddress() const {
+  return address_;
+}
+int64_t Person::getPesel() const {
+  return pesel_;
+}
+Gender Person::getSex() const {
+  return sex_;
+}
--- student.cpp
+++ student.cpp
@@ -1,1 +1,25 @@
 #include "student.hpp"
+Student::Student(const std::string &name,
+                 const std::string &surname,
+                 const std::string &address,
+                 size_t index,
+                 int64_t pesel,
+                 Gender sex)
+  : Person(name, surname, address, pesel, sex), index_(index)
+{
+
+}
+
+void Student::print() {
+  std::cout<<"Indeks:   "<<getIndex()<<std::endl;
+  std::cout<<"Imię:     "<<getName()<<std::endl;
+  std::cout<<"Nazwisko: "<<getSurname()<<std::endl;
+  std::cout<<"Adres:    "<<getAddress()<<std::endl;
+  std::cout<<"Pesel:    "<<getPesel()<<std::endl;
+  std::cout<<"Płeć:     "<<(getSex() == Gender::Female ? "Kobieta" : "Mężczyzna")
+            <<std::endl;
+  std::cout<<"----------------------------------\n";
+}
+size_t Student::getIndex() const {
+  return index_;
+}
