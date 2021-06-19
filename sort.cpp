93e9d44dadf62d00fd3fa8b092b173a7b2d0cc81
Ireneusz Bohatyrewicz <ibohatyrewicz@gmail.com>
2021-06-19 18:13:48 +0200

    modified the Sort class

--- sort.cpp
+++ sort.cpp
@@ -1,1 +1,11 @@
 #include "sort.hpp"
+#include "university.hpp"
+#include <algorithm>
+
+Sort::Sort(University* university, const std::function<bool(Person*,Person*)>& fn)
+: university_(university), fn_(fn) {
+
+}
+void Sort::execute() {
+  std::sort(university_->begin(), university_->end(), fn_);
+}
