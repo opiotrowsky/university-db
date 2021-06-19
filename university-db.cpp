3fc3a477034a4c1888b185774d0e30c5ef7ec6cf
Ireneusz Bohatyrewicz <ibohatyrewicz@gmail.com>
2021-06-19 18:35:37 +0200

    Fixed run exe

--- university-db.cpp
+++ university-db.cpp
@@ -2,14 +2,14 @@
 
 int main() {
   std::unique_ptr<University> university(new University());
-//  university->addStudent(new Student("Jan", "Kowalski", "Warszawa", 1001,
-//                                     22113355664, Gender::Male));
-//  university->addStudent(new Student("Anna", "Nowak", "Wrocław", 1002,
-//                                     12345678901, Gender::Female));
-//  university->addStudent(new Student("Andrzej", "Skowroński", "Gdańsk", 1003,
-//                                     11223344556, Gender::Male));
-  university->loadRecords();
-//  university->sortByPesel();
+  university->addStudent(new Student("Jan", "Kowalski", "Warszawa", 1001,
+                                     22113355664, Gender::Male));
+  university->addStudent(new Student("Anna", "Nowak", "Wrocław", 1002,
+                                     12345678901, Gender::Female));
+  university->addStudent(new Student("Andrzej", "Skowroński", "Gdańsk", 1003,
+                                     11223344556, Gender::Male));
+  university->saveRecords();
+  university->sortByPesel();
   university->printAllDatabase();
   return 0;
 }
